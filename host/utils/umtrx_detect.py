9d964ce08626d1177f254015cd7be2ef88acdc91
Zabbal <suarev@stud.ntnu.no>
2012-02-20 02:42:29 +0800

    HOST: simplify, improve code quality and portability

--- host/utils/umtrx_detect.py
+++ host/utils/umtrx_detect.py
@@ -15,15 +15,15 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-import re, struct, socket, subprocess
-########################################################################
-# constants
-########################################################################
+import sys, struct, socket
+# pylint: disable-msg = C0301, C0111
+
 UDP_CONTROL_PORT = 49152
 UDP_MAX_XFER_BYTES = 1024
 UDP_TIMEOUT = 3
 UDP_POLL_INTERVAL = 0.10 #in seconds
 USRP2_CONTROL_PROTO_VERSION = 11 # must match firmware proto
+UMTRX_BROADCAST = '192.168.10.255' # default UmTRX network domain
 
 #see fw_common.h
 CONTROL_FMT = '!LLL24x'
@@ -44,64 +44,37 @@ UMTRX_CTRL_ID_REQUEST = ord('u')
 UMTRX_CTRL_ID_RESPONSE = ord('U')
 USRP2_CTRL_ID_HOLLER_AT_ME_BRO = ord('l')
 
-_seq = -1
-def seq():
-    global _seq
-    _seq = _seq+1
-    return _seq
+def unpack_control_fmt(_str):
+    return struct.unpack(CONTROL_FMT, _str) #proto_ver, pktid, seq, data
 
-########################################################################
-# helper functions
-########################################################################
-def unpack_control_fmt(s):
-    return struct.unpack(CONTROL_FMT, s) #proto_ver, pktid, seq, data
-
-def unpack_control_ip_fmt(s):
-    return struct.unpack(CONTROL_IP_FMT, s) #proto_ver, pktid, seq, ip_addr
+def unpack_control_ip_fmt(_str):
+    return struct.unpack(CONTROL_IP_FMT, _str) #proto_ver, pktid, seq, ip_addr
 
 def pack_control_fmt(proto_ver, pktid, seq):
     return struct.pack(CONTROL_FMT, proto_ver, pktid, seq)
 
-
-########################################################################
-# interface discovery and device enumeration
-########################################################################
-def command(*args):
-    p = subprocess.Popen(args, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
-    ret = p.wait()
-    verbose = p.stdout.read().decode()
-    if ret != 0: raise Exception(verbose)
-    return verbose
-
-def unix_get_interfaces():
-    ifconfig = command("/sbin/ifconfig")
-    ip_addr_re = "cast\D*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"
-    bcasts = re.findall(ip_addr_re, ifconfig)
-    return bcasts
-
-def enumerate_devices():
-  for bcast_addr in unix_get_interfaces():
+def detect(bcast_addr):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     sock.settimeout(0.1)
     out_pkt = pack_control_fmt(USRP2_CONTROL_PROTO_VERSION, UMTRX_CTRL_ID_REQUEST, 0)
     print "[%s] Sending %d bytes: %x, '%c'\n" % (bcast_addr, len(out_pkt), USRP2_CONTROL_PROTO_VERSION, UMTRX_CTRL_ID_REQUEST)
     sock.sendto(out_pkt, (bcast_addr, UDP_CONTROL_PORT))
-    still_goin = True
-    while(still_goin):
+    while(True):
       try:
         pkt = sock.recv(UDP_MAX_XFER_BYTES)
         (proto_ver, pktid, rxseq, ip_addr) = unpack_control_ip_fmt(pkt)
         print "[%s] Received %d bytes: %x, '%c', %x, %s\n" % (bcast_addr, len(pkt), proto_ver, pktid, rxseq, socket.inet_ntoa(struct.pack("<L", socket.ntohl(ip_addr))))
       except socket.timeout:
-        still_goin = False
+          return
 
-########################################################################
-# N. B. heavily stripped-down, unportable version
-########################################################################
+# Specify address via command line for non-default broadcasting.
+
 if __name__ == '__main__':
 
-  print 'broadcasting over interfaces:'
-  enumerate_devices()
-  
+  print 'Detecting UmTRX:'
+  if len(sys.argv) > 1:
+      detect(sys.argv[1])
+  else:
+      detect(UMTRX_BROADCAST)
 
