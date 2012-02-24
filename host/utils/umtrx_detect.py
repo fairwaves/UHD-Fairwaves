cbf47283455d74dc979108d7a426ecc392f195a6
Zabbal <suarev@stud.ntnu.no>
2012-02-24 19:11:01 +0800

    HOST: groundwork for python spi read\write

--- host/utils/umtrx_detect.py
+++ host/utils/umtrx_detect.py
@@ -25,7 +25,7 @@ UDP_POLL_INTERVAL = 0.10 #in seconds
 USRP2_CONTROL_PROTO_VERSION = 11 # must match firmware proto
 UMTRX_BROADCAST = '192.168.10.255' # default UmTRX network domain
 
-#see fw_common.h
+# see fw_common.h
 CONTROL_FMT = '!LLL24x'
 CONTROL_IP_FMT = '!LLLL20x'
 
@@ -36,13 +36,23 @@ n2xx_revs = {
   0x0a11: ["n210_r4"],
   0xfa00: ["umtrx"],
   }
-
+# remember kids: drugs are bad...
 USRP2_CTRL_ID_HUH_WHAT = ord(' ')
 USRP2_CTRL_ID_WAZZUP_BRO = ord('a')
 USRP2_CTRL_ID_WAZZUP_DUDE = ord('A')
 UMTRX_CTRL_ID_REQUEST = ord('u')
 UMTRX_CTRL_ID_RESPONSE = ord('U')
+USRP2_CTRL_ID_TRANSACT_ME_SOME_SPI_BRO = ord('s')
+USRP2_CTRL_ID_OMG_TRANSACTED_SPI_DUDE = ord('S')
+USRP2_CTRL_ID_DO_AN_I2C_READ_FOR_ME_BRO = ord('i')
+USRP2_CTRL_ID_HERES_THE_I2C_DATA_DUDE = ord('I')
+USRP2_CTRL_ID_WRITE_THESE_I2C_VALUES_BRO = ord('h')
+USRP2_CTRL_ID_COOL_IM_DONE_I2C_WRITE_DUDE = ord('H')
+USRP2_CTRL_ID_GET_THIS_REGISTER_FOR_ME_BRO = ord('r')
+USRP2_CTRL_ID_OMG_GOT_REGISTER_SO_BAD_DUDE = ord('R')
 USRP2_CTRL_ID_HOLLER_AT_ME_BRO = ord('l')
+USRP2_CTRL_ID_HOLLER_BACK_DUDE = ord('L')
+USRP2_CTRL_ID_PEACE_OUT = ord('~')
 
 def unpack_control_fmt(_str):
     return struct.unpack(CONTROL_FMT, _str) #proto_ver, pktid, seq, data
@@ -66,13 +76,15 @@ def detect(bcast_addr):
             pkt = sock.recv(UDP_MAX_XFER_BYTES)
             (proto_ver, pktid, rxseq, ip_addr) = unpack_control_ip_fmt(pkt)
             print "Received %d bytes: %x, '%c', %x, %s" % (len(pkt), proto_ver, pktid, rxseq, socket.inet_ntoa(struct.pack("<L", socket.ntohl(ip_addr))))
+            return True
         except socket.timeout:
-            return
+            return False
 
 # Specify address via command line for non-default broadcasting.
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        detect(sys.argv[1])
+        target = sys.argv[1]
     else:
-        detect(UMTRX_BROADCAST)
+        target = UMTRX_BROADCAST
+    print detect(target)
