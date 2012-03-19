71893767664268bace1812e9db71c96173f4f13e
Zabbal <suarev@stud.ntnu.no>
2012-03-19 23:24:48 +0800

    HOST: add LMS registry reading

--- host/utils/umtrx_detect.py
+++ host/utils/umtrx_detect.py
@@ -28,7 +28,7 @@ UMTRX_BROADCAST = '192.168.10.255' # default UmTRX network domain
 # see fw_common.h
 CONTROL_FMT = '!LLL24x'
 CONTROL_IP_FMT = '!LLLL20x'
-
+SPI_FMT = '!LLLLLBBBB12x'
 n2xx_revs = {
   0x0a00: ["n200_r3", "n200_r2"],
   0x0a10: ["n200_r4"],
@@ -54,15 +54,34 @@ USRP2_CTRL_ID_OMG_GOT_REGISTER_SO_BAD_DUDE = ord('R')
 USRP2_CTRL_ID_HOLLER_AT_ME_BRO = ord('l')
 USRP2_CTRL_ID_HOLLER_BACK_DUDE = ord('L')
 USRP2_CTRL_ID_PEACE_OUT = ord('~')
+SPI_EDGE_RISE = ord('r')
+SPI_EDGE_FALL = ord('f')
 
-def unpack_control_fmt(_str):
-    return struct.unpack(CONTROL_FMT, _str) #proto_ver, pktid, seq, data
-
-def unpack_control_ip_fmt(_str):
-    return struct.unpack(CONTROL_IP_FMT, _str) #proto_ver, pktid, seq, ip_addr
+def unpack_format(_str, fmt):
+    return struct.unpack(fmt, _str)
 
 def pack_control_fmt(proto_ver, pktid, seq):
     return struct.pack(CONTROL_FMT, proto_ver, pktid, seq)
+
+def pack_spi_fmt(proto_ver, pktid, seq, dev, data, miso, mosi, bits, read):
+    return struct.pack(SPI_FMT, proto_ver, pktid, seq, dev, data, miso, mosi, bits, read)
+
+def read_spi(addr, lms, reg):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 0)
+    sock.settimeout(0.1) 
+    out_pkt = pack_spi_fmt(USRP2_CONTROL_PROTO_VERSION, USRP2_CTRL_ID_TRANSACT_ME_SOME_SPI_BRO, 0, lms, reg << 8, SPI_EDGE_RISE, SPI_EDGE_RISE, 16, 1)
+    sock.sendto(out_pkt, (addr, UDP_CONTROL_PORT))
+    while(True):
+        try:
+            pkt = sock.recv(UDP_MAX_XFER_BYTES)
+            pkt_list = unpack_format(pkt, SPI_FMT)
+            if pkt_list[1] != USRP2_CTRL_ID_OMG_TRANSACTED_SPI_DUDE:
+                return None
+            print ' '.join(hex(x) for x in pkt_list)
+            return pkt_list[4]
+        except socket.timeout:
+            return None
 
 def detect(bcast_addr):
     print 'Detecting UmTRX over %s:' % bcast_addr
@@ -75,7 +94,7 @@ def detect(bcast_addr):
     while(True):
         try:
             pkt = sock.recv(UDP_MAX_XFER_BYTES)
-            (proto_ver, pktid, rxseq, ip_addr) = unpack_control_ip_fmt(pkt)
+            (proto_ver, pktid, rxseq, ip_addr) = unpack_format(pkt, CONTROL_IP_FMT)
             print "Received %d bytes: %x, '%c', %x, %s" % (len(pkt), proto_ver, pktid, rxseq, socket.inet_ntoa(struct.pack("<L", socket.ntohl(ip_addr))))
             if pktid == UMTRX_CTRL_ID_RESPONSE:
                 return True
@@ -85,8 +104,8 @@ def detect(bcast_addr):
 # Specify address via command line for non-default broadcasting.
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        target = sys.argv[1]
-    else:
-        target = UMTRX_BROADCAST
-    print detect(target)
+    target = sys.argv[1] if len(sys.argv) > 1 else UMTRX_BROADCAST
+    if detect(target):
+        print '0x%X' % read_spi("192.168.10.2", 1, 118)
+        print '0x%X' % read_spi("192.168.10.2", 2, 118)
+
