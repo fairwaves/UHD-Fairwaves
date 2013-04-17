e90184bdeecb3453141e3aca5be7564052c27dcd
Alexander Chemeris <Alexander.Chemeris@gmail.com>
2013-04-18 01:00:47 +0400

    Host: BETA: A test code to talk to a synthetizer on UmSEL.
    In its current version it sends a predefined set of commands to the synthetizer (see the second #ifdef). Also there is no detection whether UmSEL is actually installed or not.

--- host/lib/usrp/dboard/db_lms.cpp
+++ host/lib/usrp/dboard/db_lms.cpp
@@ -19,6 +19,7 @@
 #include <limits>
 
 #include "../umtrx/lms6002d.hpp"
+#include "adf4350_regs.hpp"
 
 using namespace uhd;
 using namespace uhd::usrp;
@@ -100,8 +101,27 @@ public:
 
     double set_freq(dboard_iface::unit_t unit, double f) {
         if (verbosity>0) printf("db_lms6002d::set_freq(%f)\n", f);
-        double actual_freq = (unit==dboard_iface::UNIT_RX)
-                             ? lms.rx_pll_tune(26e6, f) : lms.tx_pll_tune(26e6, f);
+        unsigned ref_freq = get_iface()->get_clock_rate(dboard_iface::UNIT_LMS);
+        double actual_freq;
+        if (unit==dboard_iface::UNIT_TX) {
+            actual_freq = lms.tx_pll_tune(ref_freq, f);
+        } else if (unit==dboard_iface::UNIT_RX) {
+#if 1
+            actual_freq = lms.rx_pll_tune(ref_freq, f);
+#else
+            // New beta version of the code for UmSEL support.
+            const double umsel_if = 359.5e6;
+            double actual_lms_freq = lms.rx_pll_tune(ref_freq, umsel_if);
+            if (verbosity>0) printf("db_lms6002d::set_freq() actual_lms_freq=%f\n", actual_lms_freq);
+            double adf4350_freq = f - actual_lms_freq;
+            actual_freq = tune_adf4350(adf4350_freq);
+            if (verbosity>0) printf("db_lms6002d::set_freq() adf4350 freq=%f\n", actual_freq);
+            actual_freq += actual_lms_freq;
+            if (verbosity>0) printf("db_lms6002d::set_freq() actual_freq=%f\n", actual_freq);
+#endif
+        } else {
+            assert(!"Wrong units_t value passed to db_lms6002d::set_freq()");
+        }
         if (actual_freq<0)
             actual_freq = 0;
         if (verbosity>0) printf("db_lms6002d::set_freq() actual_freq=%f\n", actual_freq);
@@ -254,7 +274,181 @@ private:
     umtrx_lms6002d_dev lms;        // Interface to the LMS chip.
     int tx_vga1gain, tx_vga2gain;  // Stored values of Tx VGA1 and VGA2 gains.
     bool rf_loopback_enabled;      // Whether RF loopback is enabled.
+
+    // Tune ADF4350 on an UmSEL
+    double tune_adf4350(double target_freq);
 };
+
+/***********************************************************************
+ * Tuning
+ **********************************************************************/
+double db_lms6002d::tune_adf4350(double target_freq) {
+    UHD_LOGV(often) << boost::format(
+        "UmSEL tune: target frequency %f Mhz"
+    ) % (target_freq/1e6) << std::endl;
+
+    //clip the input
+    // TODO::::::::::::::::::::::::::::::::
+//    target_freq = sbx_freq_range.clip(target_freq);
+
+    //map prescaler setting to mininmum integer divider (N) values (pg.18 prescaler)
+    static const uhd::dict<int, int> prescaler_to_min_int_div = map_list_of
+        (0,23) //adf4350_regs_t::PRESCALER_4_5
+        (1,75) //adf4350_regs_t::PRESCALER_8_9
+    ;
+
+    //map rf divider select output dividers to enums
+    static const uhd::dict<int, adf4350_regs_t::rf_divider_select_t> rfdivsel_to_enum = map_list_of
+        (1,  adf4350_regs_t::RF_DIVIDER_SELECT_DIV1)
+        (2,  adf4350_regs_t::RF_DIVIDER_SELECT_DIV2)
+        (4,  adf4350_regs_t::RF_DIVIDER_SELECT_DIV4)
+        (8,  adf4350_regs_t::RF_DIVIDER_SELECT_DIV8)
+        (16, adf4350_regs_t::RF_DIVIDER_SELECT_DIV16)
+    ;
+
+    double actual_freq, pfd_freq;
+    // TODO:: May be *2? Check
+    double ref_freq = get_iface()->get_clock_rate(dboard_iface::UNIT_LMS) * 2;
+    int R=0, BS=0, N=0, FRAC=0, MOD=0;
+    int RFdiv = 1;
+    adf4350_regs_t::reference_divide_by_2_t T     = adf4350_regs_t::REFERENCE_DIVIDE_BY_2_DISABLED;
+    adf4350_regs_t::reference_doubler_t     D     = adf4350_regs_t::REFERENCE_DOUBLER_DISABLED;
+
+    //Reference doubler for 50% duty cycle
+    // if ref_freq < 12.5MHz enable regs.reference_divide_by_2
+    if(ref_freq <= 12.5e6) D = adf4350_regs_t::REFERENCE_DOUBLER_ENABLED;
+
+    //increase RF divider until acceptable VCO frequency
+    //start with target_freq*2 because mixer has divide by 2
+    double vco_freq = target_freq;
+    while (vco_freq < 2.2e9) {
+        vco_freq *= 2;
+        RFdiv *= 2;
+    }
+
+    //use 8/9 prescaler for vco_freq > 3 GHz (pg.18 prescaler)
+    adf4350_regs_t::prescaler_t prescaler = vco_freq > 3e9 ? adf4350_regs_t::PRESCALER_8_9 : adf4350_regs_t::PRESCALER_4_5;
+
+    /*
+     * The goal here is to loop though possible R dividers,
+     * band select clock dividers, N (int) dividers, and FRAC
+     * (frac) dividers.
+     *
+     * Calculate the N and F dividers for each set of values.
+     * The loop exists when it meets all of the constraints.
+     * The resulting loop values are loaded into the registers.
+     *
+     * from pg.21
+     *
+     * f_pfd = f_ref*(1+D)/(R*(1+T))
+     * f_vco = (N + (FRAC/MOD))*f_pfd
+     *    N = f_vco/f_pfd - FRAC/MOD = f_vco*((R*(T+1))/(f_ref*(1+D))) - FRAC/MOD
+     * f_rf = f_vco/RFdiv)
+     * f_actual = f_rf/2
+     */
+    for(R = 1; R <= 1023; R+=1){
+        //PFD input frequency = f_ref/R ... ignoring Reference doubler/divide-by-2 (D & T)
+        pfd_freq = ref_freq*(1+D)/(R*(1+T));
+
+        //keep the PFD frequency at or below 25MHz (Loop Filter Bandwidth)
+        if (pfd_freq > 25e6) continue;
+
+        //ignore fractional part of tuning
+        N = int(std::floor(vco_freq/pfd_freq));
+
+        //keep N > minimum int divider requirement
+        if (N < prescaler_to_min_int_div[prescaler]) continue;
+
+        for(BS=1; BS <= 255; BS+=1){
+            //keep the band select frequency at or below 100KHz
+            //constraint on band select clock
+            if (pfd_freq/BS > 100e3) continue;
+            goto done_loop;
+        }
+    } done_loop:
+
+    //Fractional-N calculation
+    MOD = 4095; //max fractional accuracy
+    FRAC = int((vco_freq/pfd_freq - N)*MOD);
+
+    //Reference divide-by-2 for 50% duty cycle
+    // if R even, move one divide by 2 to to regs.reference_divide_by_2
+    if(R % 2 == 0){
+        T = adf4350_regs_t::REFERENCE_DIVIDE_BY_2_ENABLED;
+        R /= 2;
+    }
+
+    //actual frequency calculation
+    actual_freq = double((N + (double(FRAC)/double(MOD)))*ref_freq*(1+int(D))/(R*(1+int(T)))/RFdiv);
+
+    // TODO:: get_locked!
+//    UHD_LOGV(often)
+    std::cout
+        << boost::format("UmSEL Intermediates: ref=%0.2f, outdiv=%f, fbdiv=%f") % (ref_freq*(1+int(D))/(R*(1+int(T)))) % double(RFdiv*2) % double(N + double(FRAC)/double(MOD)) << std::endl
+        << boost::format("UmSEL tune: R=%d, BS=%d, N=%d, FRAC=%d, MOD=%d, T=%d, D=%d, RFdiv=%d, LD=%s"
+            ) % R % BS % N % FRAC % MOD % T % D % RFdiv % true /* this->get_locked(unit).to_pp_string() */ << std::endl
+        << boost::format("UmSEL Frequencies (MHz): REQ=%0.2f, ACT=%0.2f, VCO=%0.2f, PFD=%0.2f, BAND=%0.2f"
+            ) % (target_freq/1e6) % (actual_freq/1e6) % (vco_freq/1e6) % (pfd_freq/1e6) % (pfd_freq/BS/1e6) << std::endl;
+
+    //load the register values
+    adf4350_regs_t regs;
+
+    // TODO:: What?
+//    if ((unit == dboard_iface::UNIT_TX) and (actual_freq == sbx_tx_lo_2dbm.clip(actual_freq)))
+//        regs.output_power = adf4350_regs_t::OUTPUT_POWER_2DBM;
+//    else
+        regs.output_power = adf4350_regs_t::OUTPUT_POWER_5DBM;
+
+    regs.frac_12_bit = FRAC;
+    regs.int_16_bit = N;
+    regs.mod_12_bit = MOD;
+    regs.prescaler = prescaler;
+    regs.r_counter_10_bit = R;
+    regs.reference_divide_by_2 = T;
+    regs.reference_doubler = D;
+    regs.band_select_clock_div = BS;
+    UHD_ASSERT_THROW(rfdivsel_to_enum.has_key(RFdiv));
+    regs.rf_divider_select = rfdivsel_to_enum[RFdiv];
+
+    regs.mute_till_lock_detect = adf4350_regs_t::MUTE_TILL_LOCK_DETECT_MUTE_ENABLED;
+    regs.charge_pump_current = adf4350_regs_t::CHARGE_PUMP_CURRENT_2_50MA;
+    regs.double_buffer = adf4350_regs_t::DOUBLE_BUFFER_ENABLED;
+    regs.muxout = adf4350_regs_t::MUXOUT_3STATE;
+    regs.low_noise_and_spur = adf4350_regs_t::LOW_NOISE_AND_SPUR_LOW_NOISE;
+
+
+
+    //write the registers
+    //correct power-up sequence to write registers (5, 4, 3, 2, 1, 0)
+    int addr;
+
+#if 0
+    for(addr=5; addr>=0; addr--){
+//        UHD_LOGV(often)
+        std::cout << boost::format(
+            "UmSEL SPI Reg (0x%02x): 0x%08x"
+        ) % addr % regs.get_reg(addr) << std::endl;
+        this->get_iface()->write_spi(
+            uhd::usrp::dboard_iface::UNIT_SYNT, spi_config_t::EDGE_RISE,
+            regs.get_reg(addr), 32
+        );
+    }
+#else
+    this->get_iface()->write_spi(uhd::usrp::dboard_iface::UNIT_SYNT, spi_config_t::EDGE_RISE, 0x0580000|5, 32);
+    this->get_iface()->write_spi(uhd::usrp::dboard_iface::UNIT_SYNT, spi_config_t::EDGE_RISE, 0x0BFF4F8|4, 32);
+    this->get_iface()->write_spi(uhd::usrp::dboard_iface::UNIT_SYNT, spi_config_t::EDGE_RISE, 0x0040000|3, 32);
+    this->get_iface()->write_spi(uhd::usrp::dboard_iface::UNIT_SYNT, spi_config_t::EDGE_RISE, 0x1006E40|2, 32);
+    this->get_iface()->write_spi(uhd::usrp::dboard_iface::UNIT_SYNT, spi_config_t::EDGE_RISE, 0x8008208|1, 32);
+    this->get_iface()->write_spi(uhd::usrp::dboard_iface::UNIT_SYNT, spi_config_t::EDGE_RISE, (325<<15)|(1<<3)|0, 32);
+#endif
+
+    //return the actual frequency
+//    UHD_LOGV(often) << boost::format(
+    std::cout << boost::format(
+        "UmSEL tune: actual frequency %f Mhz"
+    ) % (actual_freq/1e6) << std::endl;
+    return actual_freq;
+}
 
 // Register the LMS dboards
 
